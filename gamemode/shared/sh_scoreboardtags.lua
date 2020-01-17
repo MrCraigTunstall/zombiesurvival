8bf7491f9783376b3cda837c266c6785c1f72741
Craig Tunstall <tunstall.craig@yahoo.com>
2020-01-17 15:16:57 +0000

    Fixed missing CachedSigils

--- gamemode/shared/sh_scoreboardtags.lua
+++ gamemode/shared/sh_scoreboardtags.lua
@@ -2,47 +2,47 @@ local specialPeople = {
 	{
 		id = "STEAM_0:1:3307510",
 		img = "vgui/steam/games/icon_sourcesdk",
-		tooltip = translate.Get"tag_jetboom"
+		tooltip = translate.Get("tag_jetboom")
 	},
 	{
 		id = "STEAM_0:1:49624713",
 		img = "vgui/steam/games/icon_sourcesdk",
-		tooltip = translate.Get"tag_craig"
+		tooltip = translate.Get("tag_craig")
 	},
 	{
 		id = "STEAM_0:0:47758537",
 		img = "icons/cat.png",
-		tooltip = translate.Get"tag_flair"
+		tooltip = translate.Get("tag_flair")
 	},
 	{
 		id = "STEAM_0:0:18000855",
 		img = "icon16/bomb.png",
-		tooltip = translate.Get"tag_mka"
+		tooltip = translate.Get("tag_mka")
 	},
 	{
 		id = "STEAM_0:0:22379160",
 		img = "icon16/wrench_orange.png",
-		tooltip = translate.Get"tag_d3"
+		tooltip = translate.Get("tag_d3")
 	},
 	{
 		id = "STEAM_0:0:35752130",
 		img = "icon16/rainbow.png",
-		tooltip = translate.Get"tag_gabi"
+		tooltip = translate.Get("tag_gabi")
 	},
 	{
 		id = "STEAM_0:0:99299157",
 		img = "flags16/ru.png",
-		tooltip = translate.Get"tag_ru"
+		tooltip = translate.Get("tag_ru")
 	},
 	{
 		id = "STEAM_0:0:32908051",
 		img = "flags16/es.png",
-		tooltip = translate.Get"tag_es"
+		tooltip = translate.Get("tag_es")
 	},
 	{
 		id = "STEAM_0:0:150667652",
 		img = "flags16/kr.png",
-		tooltip = translate.Get"tag_ko"
+		tooltip = translate.Get("tag_ko")
 	}
 }
 
@@ -51,19 +51,19 @@ function GM:IsSpecialPerson(pl, image)
 	
 	if pl:IsBot() then
 		img = "icon16/bug.png"
-		tooltip = translate.Get"tag_bot"
+		tooltip = translate.Get("tag_bot")
 	elseif pl:IsSuperAdmin() then
 		img = "icon16/shield.png"
-		tooltip = translate.Get"tag_sa"
+		tooltip = translate.Get("tag_sa")
 	elseif pl:IsAdmin() then
 		img = "icons/shield_gray.png"
-		tooltip = translate.Get"tag_admin"
+		tooltip = translate.Get("tag_admin")
 	elseif pl:IsUserGroup("moderator") then
 		img = "icon16/award_gold_star_2.png"
-		tooltip = translate.Get"tag_mod"
+		tooltip = translate.Get("tag_mod")
 	elseif pl:IsUserGroup("member") then
 		img = "icon16/user.png"
-		tooltip = translate.Get"tag_user"
+		tooltip = translate.Get("tag_user")
 	end
 	
 	if not pl:IsBot() then
